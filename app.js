6334b01a3eee7643695a9f31a379b28a0e77eeff
AurobindoGupta <AurobindoGupta@users.noreply.github.com>
2021-03-01 17:35:58 +0530

    js text input

--- app.js
+++ app.js
@@ -2,5 +2,16 @@ console.log("YO its working still");
 // alert("something")
 // var username= prompt("Give me your name?");
 // alert("hahah"+username);
-var btn =document.querySelector("#app-but");
-btn.addEventListener("click", function clickEventHandler(){console.log("clicked")})
+var btn = document.querySelector("#app-but");
+var textinout= document.querySelector("#text-input");
+btn.addEventListener("click" , clickHandler())
+
+function clickHandler() {
+    {
+        console.log("clicked");
+       
+        
+    };
+   
+    console.log("input ye hoa",textinout.value );
+}
