4a8a1887c4befe9d8841da35a338ff13a9ff3ee6
AurobindoGupta <AurobindoGupta@users.noreply.github.com>
2021-03-01 16:56:14 +0530

    started

---
+++ app.js
@@ -0,0 +1,6 @@
+console.log("YO its working still");
+// alert("something")
+// var username= prompt("Give me your name?");
+// alert("hahah"+username);
+var btn =document.querySelector("#app-but");
+btn.addEventListener("click", function clickEventHandler(){console.log("clicked")})
