c169cdab4815c01cc3085db2cbe3e9f3324b3684
AurobindoGupta <AurobindoGupta@users.noreply.github.com>
2021-03-01 20:51:40 +0530

    textarea check

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-console.log("YO its working still");
+// console.log("YO its working still");
 // alert("something")
 // var username= prompt("Give me your name?");
 // alert("hahah"+username);
 var btn = document.querySelector("#app-but");
-var textinout= document.querySelector("#text-input");
-btn.addEventListener("click" , clickHandler())
+var textinput = document.querySelector("#text-input");
+console.log(textinput);
+btn.addEventListener("click", clickHandler);
 
 function clickHandler() {
     {
         console.log("clicked");
-       
-        
-    };
-   
-    console.log("input ye hoa",textinout.value );
+        console.log("input ye hoa", textinput.value);
+
+
+
+    }
+
+
 }
